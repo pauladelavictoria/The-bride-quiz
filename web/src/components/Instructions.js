42431b584e91a851f95a2ca2c870542994ef76ac
pauladelavictoria <pauladelavictoria@gmail.com>
2022-02-25 11:38:02 +0100

    adds insructions text

--- web/src/components/Instructions.js
+++ web/src/components/Instructions.js
@@ -7,26 +7,32 @@ const Instructions = () => {
 
   const [showInstruction, setShowInstruction] = useState(undefined);
   const renderInstruction = (ev) => {
-    setShowInstruction(ev.currentTarget.id !== showInstruction ? ev.currentTarget.id : undefined);
+    setShowInstruction(
+      ev.currentTarget.id !== showInstruction ? ev.currentTarget.id : undefined
+    );
   };
 
   return (
     <div className="instructions">
       <Header />
       <p className="instructions_text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dicta illo
-        amet, sed fugiat obcaecati labore omnis beatae quisquam optio dolor
-        saepe, architecto non illum maxime exercitationem nemo officiis esse!
+        ¡Bienvenidas a The bride Quiz! <br />
+        En este juego podrás reirte con tus amigas mientras celebras la
+        despedida de soltera. <br />
+        Hay tres pantallas, escoge la que más te llame la atención y acierta o
+        bebe. <br />
       </p>
       <button id="1" onClick={renderInstruction} className="instructions_btn">
         Juego de la ahogada
       </button>
       {showInstruction === "1" && (
         <p className="instructions_text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti aut
-          eos, modi eius fuga ea hic. Reprehenderit, consectetur saepe possimus
-          iste dignissimos enim, accusamus dolor dicta corporis, similique ea
-          inventore?
+          En esta pantalla se escoge una palabra aleatoria y una pista. <br />
+          Se trata de adivinar la palabra antes de consumir el número máximo de
+          errores. <br />
+          Escoger la pista supone sumar un error. <br />
+          En caso de no conseguirlo, hay que beber un chupito y pasar el
+          teléfono a la próxima jugadora.
         </p>
       )}
       <button id="2" onClick={renderInstruction} className="instructions_btn">
@@ -34,10 +40,12 @@ const Instructions = () => {
       </button>
       {showInstruction === "2" && (
         <p className="instructions_text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti aut
-          eos, modi eius fuga ea hic. Reprehenderit, consectetur saepe possimus
-          iste dignissimos enim, accusamus dolor dicta corporis, similique ea
-          inventore?
+          En esta pantalla aparece una pregunta con tres posibles opciones.
+          <br />
+          Se trata de adivinar cuál es la correcta. <br />
+          Solo tienes una oportunidad de acertar. <br />
+          en caso de acierto, escoge quién bebe un chupito, si no es así, bebe
+          un chupito y pasa el teléfono a la próxima jugadora.
         </p>
       )}
       <button id="3" onClick={renderInstruction} className="instructions_btn">
@@ -45,10 +53,12 @@ const Instructions = () => {
       </button>
       {showInstruction === "3" && (
         <p className="instructions_text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti aut
-          eos, modi eius fuga ea hic. Reprehenderit, consectetur saepe possimus
-          iste dignissimos enim, accusamus dolor dicta corporis, similique ea
-          inventore?
+          En esta pantalla hay que escoger entre contar una verdad o atreverse
+          con un reto. <br />
+          Pulsa el botón que prefieras para que aparezca tu verdad o reto.
+          <br />
+          En caso de no querer contar la verdad o realizar el reto, hay que
+          beber un chupito y pasar el teléfono a la próxima jugadora.
         </p>
       )}
     </div>
