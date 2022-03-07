c6a4f67fd532e8208f6330f044588a8e92576897
pauladelavictoria <pauladelavictoria@gmail.com>
2022-03-07 12:48:44 +0100

    next player screen prueba3

--- web/src/components/Pruebas/3_Prueba.js
+++ web/src/components/Pruebas/3_Prueba.js
@@ -70,84 +70,91 @@ const Prueba3 = (props) => {
   const nextWord = (props) => {
     setCurrentWord(currentWord + 1);
     setNumberOfErrors(0);
+    setArrayNotInclude([]);
+    setShowClue(false);
     props.setCurrentPlayer(props.currentPlayer + 1);
   };
 
-console.log(numberOfErrors)
-   const maxError = () => {
-     if (numberOfErrors === 5) {
-       return (
-         <>
-        <Dummy numberOfErrors={numberOfErrors} />
-        <p className="finishContainer_text">Oh! Has perdido, bebe un chupito y pasa el móvil a la siguiente</p>
-        <button onClick={nextWord} className="clueBtn">
-              Siguiente palabra
-            </button>
-        </>
-       )
-     } else {
-       return (renderGame())
-     }
-   }
-  
+  // Función para mostrar e botón de siguiente palabra
+  // cuando se llega a 5 fallos y se pierde el juego
+  const maxError = () => {
+    if (numberOfErrors === 5) {
+      return (
+        <div className="final">
+          <Dummy numberOfErrors={numberOfErrors} />
+          <p className="finishContainer_text final_text">
+            Oh! Has perdido, bebe un chupito y pasa el móvil a la siguiente
+          </p>
+          <button onClick={nextWord} className="final_btn clueBtn">
+            Siguiente palabra
+          </button>
+        </div>
+      );
+    } else {
+      return renderGame();
+    }
+  };
+
   //Función para pintar el juego
   const renderGame = () => {
-      return (
-        <>
-        {props.nextPlayer()} 
-          <main className="container">
-          
-            <h1 className="container_title">Ahogada</h1>
-           
+    return (
+      <>
+        {props.nextPlayer()}
+        <main className="container">
+          <h1 className="container_title">Ahogada</h1>
 
-            <section>
-              <SolutionLetters renderSolutionLetters={renderSolutionLetters} />
+          <section>
+            <SolutionLetters renderSolutionLetters={renderSolutionLetters} />
 
-              <form className="form">
-                <input
-                  autoComplete="off"
-                  className="form__input"
-                  maxLength="1"
-                  autoFocus
-                  type="text"
-                  name="last-letter"
-                  id="last-letter"
-                  onChange={handleKeyUp}
-                  value={lastLetter}
-                />
-              </form>
-            </section>
-            <Dummy numberOfErrors={numberOfErrors} />
-          </main>
-          <div className="error">
-            <div className="error_flex">
-              <h2 className="error_title">Letras falladas:</h2>
-              <ul className="error_letters">
-                {arrayNotInclude.map((eachLetter, index) => {
-                  return (
-                    <li key={index} className="error_letter">
-                      {eachLetter}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <button onClick={toggleShowClue} className="clueBtn">
-              Pista!
-            </button>
-            {showClue && (
-              <p className="container_text">{words[currentWord]?.clue}</p>
-            )}
+            <form className="form">
+              <input
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                autoFocus
+                type="text"
+                name="last-letter"
+                id="last-letter"
+                onChange={handleKeyUp}
+                value={lastLetter}
+              />
+            </form>
+          </section>
+          <Dummy numberOfErrors={numberOfErrors} />
+        </main>
+        <div className="error">
+          <div className="error_flex">
+            <h2 className="error_title">Letras falladas:</h2>
+            <ul className="error_letters">
+              {arrayNotInclude.map((eachLetter, index) => {
+                return (
+                  <li key={index} className="error_letter">
+                    {eachLetter}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-        </>
-      );
+          <button onClick={toggleShowClue} className="clueBtn">
+            Pista!
+          </button>
+          {showClue && (
+            <p className="container_text">{words[currentWord]?.clue}</p>
+          )}
+        </div>
+      </>
+    );
   };
 
   return (
     <div className="page">
       <Header />
-      {(currentWord < props.numberOfPlayers) ? maxError() : <FinishScreen players={props.players}/>}
-     </div>
+      {currentWord < props.numberOfPlayers ? (
+        maxError()
+      ) : (
+        <FinishScreen players={props.players} />
+      )}
+    </div>
   );
 };
 
